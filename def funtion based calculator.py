00fbdfc03d8cda074acb223d62e7323b83c632de
krins-2404 <133010134+krins-2404@users.noreply.github.com>
2026-03-18 03:01:12 +0000

    Functions
    user input function based the  basic calculator

---
+++ def funtion based calculator.py
@@ -0,0 +1,30 @@
+x= int(input("The Enter first number:"))
+y= int(input("The Enter Second Number:"))
+def add(x,y):
+    return x+y
+
+print ("The Addition of x+y:",add(x,y))
+
+
+def subtract(x,y):
+    return x-y
+
+print ("The Subtraction of x-y:",subtract(x,y))
+
+
+def multiple(x,y):
+    return x*y
+
+print ("The Multiplication of x*y:",multiple(x,y))
+
+
+def divide(x,y):
+    return x/y
+
+print ("The Division of x/y:",divide(x,y))
+
+
+def average(x,y):
+    return (x+y)/2
+
+print ("The Average of x and y:",average(x,y))
