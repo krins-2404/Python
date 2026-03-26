71bddc43ecdd3747074ab5da764895111af72ab8
krins-2404 <133010134+krins-2404@users.noreply.github.com>
2026-03-26 04:20:59 +0000

    Basic division program
    Divison performed with if - else statement.

---
+++ basic division with if-else statement.py
@@ -0,0 +1,9 @@
+
+#division
+x = float(input("Enter the dividend for division:"))
+y = float(input("Enter the divisor for division:"))
+if y == 0 :
+    print("Error:Divided by zero is not allowed")
+else:
+    z = x/y
+    print("The result of division is:",z)
